c273e0e009839b921d6ca6e6bc651722a1bac8f2
Purva0020 <purvasarode20@gmail.com>
2026-06-27 23:56:45 +0530

    added two files

--- Homework_day_2/q1.js
+++ Homework_day_2/q1.js
@@ -1,7 +1,19 @@
-const arr=["tv","phone","laptop"]
-const[t,p,l]=arr
-console.log(arr[0]);
-console.log(t);
+const products=["tv","phone","laptop"]
+const[t,p,l]=products
+console.log(products[0]);
+console.log(t +" " +p +" " +l);
+
+
+const product={
+    name:"tv",
+    price:8000,
+    company:"TCL"
+}
+
+const{name,price,company}=product;
+console.log("name" +name);
+console.log("price: " +price);
+console.log("company :" +company);
 
 
 
---
+++ Homework_day_2/q2.js
@@ -0,0 +1,15 @@
+class product{
+    name="phone"
+    productId=10
+    price=10000
+
+  displayInfo(){
+    console.log(this.name);
+    console.log(this.productId);
+    console.log(this.price);
+    }
+
+}
+
+const a= new product
+a.displayInfo()
